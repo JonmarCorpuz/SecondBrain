1bd7e780445a74eea6fa6bf5a0406ea566bbb545
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-24 19:12:23 -0400

    Update expressions.tf

--- Automation/Terraform/Syntax/expressions.tf
+++ Automation/Terraform/Syntax/expressions.tf
@@ -1,1 +1,9 @@
+# Produce a tuple
+[for <key> in var.<variable_id> : <actions>]
+
+# Produce an object
+{for <key> in var.<variable_id> : <actions>}
+
+# Map or transform elements from a collection into a new form
+{for <key> in var.<variable_id> : <key> => <new_value> [conditions]}
 
